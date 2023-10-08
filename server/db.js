c6cfeda841d615235da78714a3719b011779c083
atharva26aug <57860716+atharva26aug@users.noreply.github.com>
2023-10-09 00:34:52 +0530

    Update db.js

--- server/db.js
+++ server/db.js
@@ -1,14 +1,5 @@
 const Pool = require("pg").Pool;
 
-// const pool = new Pool({
-//     user: "postgres", 
-//     password: "mdEvNlDCJKMCPyd4GXA4", 
-//     host: "containers-us-west-197.railway.app",
-//     port: 7362, 
-//     database: "railway"
-// });
-
-
 const pool = new Pool({
     user: process.env.PGUSER, 
     password: process.env.PGPASSWORD, 
